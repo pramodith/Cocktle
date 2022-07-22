ab89f738d3b3e5889137ac1a297f43fd8035ee66
pramodith <pramodith1@gmail.com>
2022-07-22 16:33:54 -0400

    Reorganizing some components to show guess history and just a single textbox.

--- src/curdle_app.py
+++ src/curdle_app.py
@@ -6,9 +6,12 @@ from utils import *
 st.title("Curdle : So you think you know your drinks? :beer: ")
 final_message = st.empty()
 st.subheader("Ingredients")
-clue_text = st.empty()
-ingredients_table = st.empty()
-final_image = st.empty()
+qna_col, history_col = st.columns([0.75,0.25])
+
+with qna_col:
+    clue_text = st.empty()
+    ingredients_table = st.empty()
+    final_image = st.empty()
 
 def get_animated_image(image):
     for i in range(1, 30):
@@ -16,50 +19,69 @@ def get_animated_image(image):
         time.sleep(0.05)
         final_image.image(new_image)
 
+def df_index_from_1(df : pd.DataFrame):
+    df.index = [i+1 for i in range(len(df))]
+    return df
+
 if 'curr_index' not in st.session_state:
     st.session_state['curr_index'] = 0
+    st.session_state['guess_history'] = []
+    curr_index = 0
     df = pd.read_csv("../data/cocktail_ingredients.csv")
     row = df.sample(1).reset_index()
     clues = row.loc[0, 'Ingredients'].split("|")
-    cocktail = row.loc[0,"Cocktails"]
+    cocktail = row.loc[0, "Cocktails"]
     st.session_state['clues'] = clues
     st.session_state['cocktail'] = cocktail
     for i in range(len(clues)):
-        st.session_state[str(i)] = ""
+        st.session_state[i] = ""
     clue_text.write(f"Clue {st.session_state['curr_index']+1} : {clues[st.session_state['curr_index']]}")
     print(cocktail)
 
 else:
     clues = st.session_state['clues']
     cocktail = st.session_state['cocktail']
+    curr_index = st.session_state['curr_index']
+    print(st.session_state.to_dict())
 
-empty_containers = [st.empty() for i in range(len(clues))]
-guesses = [empty_containers[i].text_input(label=f"Guess {i+1}",key=i, value=st.session_state[i], disabled=st.session_state[i]!="") for i in range(len(empty_containers))]
-if st.session_state['curr_index']<len(clues) and st.session_state[st.session_state['curr_index']].lower() == cocktail.lower():
-    ingredients_table.table(pd.DataFrame(clues, columns=["Ingredient"]))
+with history_col:
+    guess_table = st.empty()
+
+
+if curr_index<len(clues) and st.session_state[curr_index].lower() == cocktail.lower():
+    ingredients_table.table(df_index_from_1(pd.DataFrame(clues, columns=["Ingredient"]).reset_index(drop=True)))
     final_message.markdown(f"Great job! Time to chug that drink :cocktail: ")
     image = get_image(cocktail)
     get_animated_image(image)
+    guess_table.table(df_index_from_1(pd.DataFrame(st.session_state['guess_history'], columns=["Guesses"])))
 
-elif st.session_state[st.session_state['curr_index']] in guesses[:st.session_state['curr_index']]:
+elif st.session_state[curr_index] in st.session_state['guess_history']:
     clue_text.write(f"Bro are you drunk? You can't guess the same drink twice! \n "
-                    f"Clue {st.session_state['curr_index'] + 1} : {clues[st.session_state['curr_index']]}")
-    empty_containers[st.session_state['curr_index']].text_input(label=f"Guess {st.session_state['curr_index']+1}"
-                                                                ,key = st.session_state['curr_index'], disabled=False,value="")
+                    f"Clue {curr_index + 1} : {clues[curr_index]}")
+    guess_table.table(df_index_from_1(pd.DataFrame(st.session_state['guess_history'], columns=["Guesses"])))
 
 else:
-    if st.session_state[st.session_state['curr_index']] != "":
-        st.session_state['curr_index'] += 1
-        if st.session_state['curr_index'] == len(clues):
-            ingredients_table.table(pd.DataFrame(clues, columns=["Ingredient"]))
+    if st.session_state[curr_index] != "":
+        st.session_state['guess_history'].append(st.session_state[curr_index])
+        guess_table.table(df_index_from_1(pd.DataFrame(st.session_state['guess_history'], columns=["Guesses"])))
+        curr_index += 1
+        st.session_state['curr_index'] = curr_index
+        if curr_index == len(clues):
+            ingredients_table.table(df_index_from_1(pd.DataFrame(clues, columns=["Ingredient"]).reset_index(drop=True)))
             final_message.markdown(f"Bro do you even drink? :grin: "
                                    f"The right answer is __{cocktail}__ ")
             image = get_image(cocktail)
             get_animated_image(image)
         else:
-            if st.session_state['curr_index'] < len(clues):
-                if levenshtein_distance(cocktail.lower(),st.session_state[st.session_state['curr_index']-1].lower())<3:
+            if curr_index < len(clues):
+                if levenshtein_distance(cocktail.lower(),st.session_state[curr_index-1].lower())<3:
                     clue_text.write(f"You are very close to the right answer!!! \n"
-                                    f"Clue {st.session_state['curr_index'] + 1} : {clues[st.session_state['curr_index']]}")
+                                    f"Clue {curr_index + 1} : {clues[curr_index]}")
                 else:
-                    clue_text.write(f"Clue {st.session_state['curr_index'] + 1} : {clues[st.session_state['curr_index']]}")
+                    clue_text.write(f"Clue {curr_index + 1} : {clues[curr_index]}")
+
+with qna_col:
+    if curr_index<len(clues) and st.session_state[curr_index].lower() != cocktail.lower():
+        response = st.empty()
+        response.text_input(label=f"Guess {curr_index+1}", key=curr_index, value="")
+
